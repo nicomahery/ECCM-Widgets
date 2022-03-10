859cacd8ae987497aaa626043ac6d72cc4c336ef
nicomahery <nicomahery@gmail.com>
2022-03-10 17:18:57 +0100

    export added to main

--- lib/eccm_widget.dart
+++ lib/eccm_widget.dart
@@ -1,7 +1,3 @@
 library eccm_widget;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'widgets/DemiCircularGaugeWidget.dart';
